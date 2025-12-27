51f1561bbbd92c65ba31d0e3f1af646c6b58de2b
ckjavahelpmate <ck.javahelpmate@gmail.com>
2025-12-28 01:09:44 +0530

    Helper funstions updated for verify file download and multiple tab handling

--- tests/script-practice/VerifyFileDownload.test.js
+++ tests/script-practice/VerifyFileDownload.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import fs from 'fs' ;
+import fs from 'fs';
 
 test.describe("Verify file download", { tag: "@regression" }, () => {
     test("Verify File download", async ({ page }) => {
@@ -8,23 +8,51 @@ test.describe("Verify file download", { tag: "@regression" }, () => {
         await page.waitForTimeout(3000)
 
         // Set event listener
-        const downloadPromise = page.waitForEvent('download'); 
+        const downloadPromise = page.waitForEvent('download');
 
         // Click on download link
         await page.getByText("some-file.txt").click();
 
         // Extract file from promise
-        const download = await downloadPromise ;
+        const download = await downloadPromise;
 
         //get filename
-        const filePath =  `downloads/${download.suggestedFilename()}` ;
+        const filePath = `downloads/${download.suggestedFilename()}`;
 
         //save file
         await download.saveAs(filePath);
 
         //Verify the file exists or not
-        expect( fs.existsSync(filePath)).toBe(true) ;
+        expect(fs.existsSync(filePath)).toBe(true);
 
 
     })
 })
+
+test.describe("Verify file download Using helper funstion", { tag: "@regression" }, () => {
+    test("Verify File download using helper funstion", async ({ page }) => {
+
+        await page.goto("https://the-internet.herokuapp.com/download")
+        await page.waitForTimeout(3000)
+
+        // download and save file
+        const filePath = await downloadFile(page, async () => {
+            await page.getByText("some-file.txt").click();
+        }, 'downloads');
+
+        console.log(filePath)
+
+        //Verify the file exists or not
+        expect(fs.existsSync(filePath)).toBe(true);
+    })
+})
+
+// Helper function
+async function downloadFile(page, trigerDownload, dir) {
+    const downloadPromise = page.waitForEvent('download')
+    await trigerDownload();
+    const download = await downloadPromise;
+    const filePath = `${dir}/${download.suggestedFilename()}`
+    await download.saveAs(filePath);
+    return filePath;
+}
---
+++ tests/script-practice/multipleTabsExample.test.js
@@ -0,0 +1,52 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("Multiple Tabs Example", { tag: "@regression" }, () => {
+    test("Multiple tabs Example", async ({ page }) => {
+        await page.goto("https://demoapps.qspiders.com/")
+        await page.waitForTimeout(3000)
+
+        await page.getByText('UI Testing Concepts').click();
+        await page.waitForTimeout(3000);
+
+        await page.getByText("Popups").click();
+        await page.waitForTimeout(3000);
+
+        await page.getByText("Browser Windows").click();
+        await page.waitForTimeout(3000);
+
+        const newPage = await openNewTab(page.context(), async () => {
+            await page.locator(".p-4", {
+                has: page.getByText("Watches")
+            }).getByText("view more").click();
+        })
+        await newPage.waitForTimeout(3000);
+
+        await newPage.getByRole('button', { name: "Add to Cart" }).click();
+        await newPage.waitForTimeout(3000);
+
+        await newPage.close();
+        await page.waitForTimeout(3000);
+
+
+        const np = await openNewTab(page.context(), async () => {
+            await page.locator(".p-4", {
+                has: page.getByText("Laptop")
+            }).getByText("view more").click();
+        })
+        await np.waitForTimeout(3000);
+
+        await np.close();
+        await page.waitForTimeout(3000);
+
+
+    })
+})
+
+// Helper function
+async function openNewTab(context, action) {
+    const newPagePromise = context.waitForEvent('page');
+    await action();
+    const newPage = await newPagePromise;
+    await newPage.waitForLoadState();
+    return newPage;
+}
